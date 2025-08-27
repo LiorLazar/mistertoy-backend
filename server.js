ba69fbd6b0f2c04470b1446c7b414f6bda41ffb6
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-08-27 20:23:22 +0300

    Init basic server

---
+++ server.js
@@ -0,0 +1,31 @@
+import path from 'path'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import { loggerService } from './services/logger.service.js'
+
+const app = express()
+
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:8080',
+        'http://localhost:8080',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173'
+    ],
+    credentials: true
+}
+
+// Express Config
+app.use(express.static('public'))
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.json())
+app.set('query parser', 'extended')
+
+
+const PORT = process.env.PORT || 3030
+app.listen(PORT, () =>
+    loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`)
+)
