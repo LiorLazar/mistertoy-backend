2fcf0eab71e085262bbc37271b35f6c582dea056
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-10-19 09:01:31 +0300

    Fix DB issue

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://liorlazar3_db_user:vJgD4UcdeOzMVZMX@mister-toy.rvpzm1y.mongodb.net/',
+    dbURL: 'mongodb+srv://liorlazar3_db_user:vJgD4UcdeOzMVZMX@mister-toy.rvpzm1y.mongodb.net/mistertoy_db?retryWrites=true&w=majority',
     dbName: 'mistertoy_db'
 }
